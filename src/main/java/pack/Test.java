5e95077882be2dde891346d47498f93bab864694
HP <HP@DESKTOP-VB3F1TD>
2024-09-22 13:31:15 +0530

    Swwaping code addede

--- src/main/java/pack/Test.java
+++ src/main/java/pack/Test.java
@@ -5,5 +5,19 @@ public class Test {
 	public static void main(String[]args) {
 		String str = "Git Demo";
 		System.out.println("Print in console:" +str);
+		
+		String a= "Hellow";
+		String b= "Word";
+		
+		System.out.println("Before swap a = "+ a + " and b =" + b);
+		a=a+b;
+		b=a.substring(0,a.length()-b.length());
+		
+		a=a.substring( b.length());
+		
+		System.out.println("After swap= a "+a);
+		System.out.println("After swap= b "+b);
+
+	
 	}
 }
